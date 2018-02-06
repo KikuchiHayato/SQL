b45a6ca436f4801afbbadd395be1b1929fc17bea
LAPTOP-5JFKR3UE\kikuchi <s.arutria@gmail.com>
2018-02-06 19:00:11 +0900

    SQL　問い4まで完了

---
+++ Q3.sql
@@ -0,0 +1,4 @@
+INSERT INTO item_category (category_id,category_name) VALUES (1,'â∆ãÔ');
+INSERT INTO item_category (category_id,category_name) VALUES (2,'êHïi');
+INSERT INTO item_category (category_id,category_name) VALUES (3,'ñ{');
+ 
