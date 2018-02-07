66eba5ab8dadbbbf518ee1fcf1d0a9eab8457931
LAPTOP-5JFKR3UE\kikuchi <s.arutria@gmail.com>
2018-02-07 13:31:07 +0900

    SQL終了

---
+++ Q9.sql
@@ -0,0 +1,2 @@
+SELECT category_name,SUM(item_price) AS total_price FROM item INNER JOIN item_category on item.category_id = item_category.category_id GROUP BY item.category_id ORDER BY total_price DESC;
+ 
